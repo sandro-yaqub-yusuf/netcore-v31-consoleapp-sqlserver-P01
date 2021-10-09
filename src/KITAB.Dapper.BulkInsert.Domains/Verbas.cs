49397334c71e5a95e52d014f15fd1d7cb14c0ae3
sandro-yaqub-yusuf <sandroyy@gmail.com>
2021-10-09 17:37:56 -0300

    Criação do projeto
    Criação do projeto

---
+++ src/KITAB.Dapper.BulkInsert.Domains/Verbas.cs
@@ -0,0 +1,10 @@
+﻿namespace KITAB.Dapper.BulkInsert.Domains
+{
+    public class Verbas
+    {
+		public int id_holerite { get; set; }
+		public double valor { get; set; }
+		public string descricao_verba { get; set; }
+		public int codverba { get; set; }
+	}
+}
